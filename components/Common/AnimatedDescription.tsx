eeca56d6daff1769c950ef89d8a35c3a05fb97ec
John Timilsina <johntimilsina@gmail.com>
2025-02-22 11:15:29 +1100

    feat: update animated texts in the login page

--- components/Common/AnimatedDescription.tsx
+++ components/Common/AnimatedDescription.tsx
@@ -7,13 +7,13 @@ export default function AnimatedDescription() {
         <div className="text-2xl md:text-3xl lg:text-4xl font-semibold text-slate-700 dark:text-slate-200">
             <TypeAnimation
                 sequence={[
-                    "Discover new movies",
+                    "Discover new movies.",
                     2000,
-                    "Get personalized recommendations",
+                    "Get personalized AI recommendations.",
                     2000,
-                    "Share your favorites with friends",
+                    "Share your favorites with friends.",
                     2000,
-                    "Never run out of things to watch",
+                    "Never run out of things to watch.",
                     2000,
                 ]}
                 wrapper="span"
